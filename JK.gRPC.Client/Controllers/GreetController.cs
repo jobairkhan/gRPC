daeea2ff9a3232cd9fc6a9cc6e23cc5fc1f90793
Jobair Khan <jobair.khan@peachvideo.com>
2021-06-27 15:35:41 +0100

    init gRPC in ASP.NET Core

---
+++ JK.gRPC.Client/Controllers/GreetController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using JK.gRPC.Server;
+
+namespace JK.gRPC.Client.Controllers
+{
+[ApiController]
+[Route("[controller]")]
+public class GreetController : ControllerBase
+{
+    private readonly Greeter.GreeterClient _client;
+
+    public GreetController(Greeter.GreeterClient client)
+    {
+        _client = client;
+    }
+
+    [HttpGet("{name}")]
+    public IActionResult Get([FromRoute]string name, CancellationToken cancellation)
+    {
+        var request = new HelloRequest
+        {
+            Name = name
+        };
+        var call = _client.SayHello(request, cancellationToken: cancellation);
+
+        return Ok(call.Message);
+    }
+}
+}
